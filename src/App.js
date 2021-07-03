3c7d25571461baf85348fe103b601da9230f68a1
Khalil <khalil.chettaoui06@gmail.com>
2021-07-03 16:44:09 +0100

    styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import { Switch, Route } from 'react-router';
+import React from "react";
+import "./App.css";
+import { Switch, Route } from "react-router";
 import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from './pages/shop/shop.component';
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component.jsx";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
       </Switch>
-
     </div>
-
   );
 }
 
