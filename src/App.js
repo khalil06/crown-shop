69edb680b5f85414f60a8fa5bcce6a6b5101eda5
Khalil <khalil.chettaoui06@gmail.com>
2021-07-03 10:57:06 +0100

    collection-preview Component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
-import HomePage from "./pages/homepage/homepage.component"
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hello" component={hello} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
 
     </div>
 
   );
 }
-const hello = () => (
-  <h1>Hello</h1>
-)
 
 export default App;
